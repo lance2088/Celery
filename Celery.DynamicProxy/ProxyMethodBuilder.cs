d4b07a44f911e272dd6d395f1441ed270565235a
Luky Wong <lukywong@gmail.com>
2013-09-19 01:55:05 +0800

    Add push args method

--- Celery.DynamicProxy/ProxyMethodBuilder.cs
+++ Celery.DynamicProxy/ProxyMethodBuilder.cs
@@ -46,10 +46,10 @@ namespace Celery.DynamicProxy
 
             ILGenerator ilGenerator = methodBuilder.GetILGenerator();
 
-            BuildProxyMethodBody(ilGenerator, methodInfo);
+            BuildProxyMethodBody(methodInfo, ilGenerator);
         }
 
-        private void BuildProxyMethodBody(ILGenerator il, MethodInfo methodInfo)
+        private void BuildProxyMethodBody(MethodInfo methodInfo, ILGenerator il)
         {
             //if (Intercepter == null)
             //{
@@ -86,7 +86,87 @@ namespace Celery.DynamicProxy
             il.Emit(OpCodes.Call, ReferenceData.GetMethodFromHandle);
             il.Emit(OpCodes.Castclass, typeof(MethodInfo));
 
+            PushGenericArgs(methodInfo, il);
+            PushParameters(methodInfo, il);
+        }
 
+        private void PushGenericArgs(MethodInfo methodInfo, ILGenerator il)
+        {
+            Type[] genericArgs = methodInfo.GetGenericArguments();
+
+            int genArgsCount = 0;
+            if (genericArgs != null)
+            {
+                genArgsCount = genericArgs.Length;
+            }
+            //Type[] genericArgs = new Type[genArgsCount]
+            il.Emit(OpCodes.Ldc_I4, genArgsCount);
+            il.Emit(OpCodes.Newarr, typeof(Type));
+
+            if (genArgsCount > 0)
+            {
+                for (int i = 0; i < genArgsCount; i++)
+                {
+                    Type currentType = genericArgs[i];
+
+                    il.Emit(OpCodes.Dup);
+                    il.Emit(OpCodes.Ldc_I4, i);
+                    il.Emit(OpCodes.Ldtoken, currentType);
+                    il.Emit(OpCodes.Call, ReferenceData.GetMethodFromHandle);
+                    il.Emit(OpCodes.Stelem_Ref);
+                }
+            }
+        }
+
+        private void PushParameters(MethodInfo methodInfo, ILGenerator il)
+        {
+            ParameterInfo[] paramInfos = methodInfo.GetParameters();
+            int paramCount = 0;
+            if (paramInfos != null)
+            {
+                paramCount = paramInfos.Length;
+            }
+            il.Emit(OpCodes.Ldc_I4, paramCount);
+            il.Emit(OpCodes.Newarr, typeof(object));
+
+            if (paramCount == 0)
+            {
+                //il.Emit(OpCodes.Ldloc_S, 0);
+                return;
+            }
+
+            il.Emit(OpCodes.Stloc_S, 0);
+
+            int i = 0;
+            int paramPos = 1;
+            foreach (ParameterInfo param in paramInfos)
+            {
+                Type paramType = param.ParameterType;
+                // args[i] = arguments[i]
+                il.Emit(OpCodes.Ldloc_S, 0); //push the reference of args array onto evaluation stack
+                il.Emit(OpCodes.Ldc_I4, i);  //push index onto the evaluation stack
+                if (param.IsOut)
+                {
+                    il.Emit(OpCodes.Ldnull); //push null onto the evaluation stack
+                    //pop the value, index, and array reference, 
+                    //the value is put into the array element at given index i
+                    il.Emit(OpCodes.Stelem_Ref);
+                    paramPos++;
+                    i++;
+                    continue;
+                }
+                il.Emit(OpCodes.Ldarg, paramPos); //load next arg at paramPos position onto evaluation stack
+                if (paramType.IsValueType) //if it is value type, box it and push to stack
+                    il.Emit(OpCodes.Box, paramType);
+
+                //pop the value, index, and array reference, 
+                //the value is put into the array element at given index i
+                il.Emit(OpCodes.Stelem_Ref);
+
+                i++;
+                paramPos++;
+            }
+            il.Emit(OpCodes.Ldloc_S, 0);
         }
     }
 }
